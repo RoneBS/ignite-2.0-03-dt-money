2ac654193d6820ec19051854798b9c2de214c3cb
Rone Silveira <ronebs10@hotmail.com>
2022-08-25 14:58:19 -0300

    Component NewTransactionModal added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,21 @@
 import logoImg from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import * as S from './styles'
+import { NewTransactionModal } from '../NewTransactionsModal'
 
 export const Header = () => {
   return (
     <S.HeaderContainer>
       <S.HeaderContent>
         <img src={logoImg} alt="" />
-        <S.NewTransactionButton>Nova Transação</S.NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <S.NewTransactionButton>Nova Transação</S.NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </S.HeaderContent>
     </S.HeaderContainer>
   )
